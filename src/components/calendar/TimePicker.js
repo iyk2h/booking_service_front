d58c6718363d192ff3cb8a7cc11586b0b265f66b
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-05-04 23:51:34 +0900

    add timeBtn markup & css

--- src/components/calendar/TimePicker.js
+++ src/components/calendar/TimePicker.js
@@ -1,8 +1,12 @@
 import { useDateState, useDateDispatch } from "../../context/dateContext";
-import { fullDateFormatter } from "../../utils/format";
+import { fullDateFormatter, timeFormatter, range } from "../../utils/format";
 import { getReservedTime } from "../../apis/api";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
+import styled from "styled-components";
+
+const startTime = 8;
+const endTime = 19;
 
 function TimePicker() {
   const urlParams = useParams();
@@ -14,18 +18,52 @@ function TimePicker() {
   }, [dateState.viewDate, urlParams.fno]);
 
   async function getReservedTimeByDate(fno, dateState) {
-    if(!dateState.viewDate) return;
+    if (!dateState.viewDate) return;
     try {
-      const date = fullDateFormatter(dateState.viewYear, dateState.viewMonth, dateState.viewDate);
+      const date = fullDateFormatter(
+        dateState.viewYear,
+        dateState.viewMonth,
+        dateState.viewDate
+      );
       const res = await getReservedTime(urlParams.fno, date);
-      dateDispatch({ type: "SET_TIME", payload: res.data});
+      dateDispatch({ type: "SET_TIME", payload: res.data });
     } catch (err) {
-      console.log(`${err} \n --- 클릭한 날짜의 이미 예약된 시간 받아올때 에러 ---`);
+      console.log(
+        `${err} \n --- 클릭한 날짜의 이미 예약된 시간 받아올때 에러 ---`
+      );
     }
   }
-  console.log(dateState.reservedTime)
-  return <div></div>;
+  console.log(dateState.reservedTime);
+
+  // 이제 state를 기반으로 시간 버튼 렌더링하기.
+  const timeBtnList = range(startTime, endTime).map(time => {
+    return <StTimeBtn key={time}>{timeFormatter(time)}</StTimeBtn>
+  })
+  return (
+    <StTimeContainer>
+      {timeBtnList}
+    </StTimeContainer>
+  );
 }
 
+const StTimeContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-flow: row wrap;
+  margin: 20px 10px;
+`;
+
+const StTimeBtn = styled.div`
+  &:hover {
+    cursor: pointer;
+  }
+  width: calc(100% / 5);
+  text-align: center;
+  border-radius: 5px;
+  border: 1px solid grey;
+  border-radius: 5px;
+  padding: 0.3em 0;
+  margin: 0.5em;
+`;
 
 export default TimePicker;
--- src/utils/format.js
+++ src/utils/format.js
@@ -1,11 +1,18 @@
+// ex) 1 -> 01
 function dateFormatter(date) {
   return Number(date) < 10 ? `0${date}` : date;
 }
 
+// ex) 2022,5,1 -> 2022-05-01
 export function fullDateFormatter(year, month, date) {
   return {
     date: `${year}-${dateFormatter(month)}-${dateFormatter(date)}`,
   };
+}
+
+// ex) 12 -> 12:00
+export function timeFormatter(time) {
+  return Number(time) < 10 ? `0${time}:00` : `${time}:00`;
 }
 
 export function range(start, end) {
