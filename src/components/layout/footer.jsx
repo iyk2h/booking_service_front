14db0dcafe06684a81490c656251dce049a7e4eb
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-05-04 22:53:46 +0900

    add survey link on footer

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,12 +1,40 @@
-import React from "react";
 import styled from "styled-components";
 
+const surveyLink = "https://naver.me/FIp9ILfg";
+
 export default function Footer() {
-  return <Wrap>© 목대컴공</Wrap>;
+  return (
+    <Wrap>
+      <div style={{opacity: 0.4, fontSize: "12px"}}>© 목대컴공</div>
+      <SurveyLink />
+    </Wrap>
+  );
+}
+
+function SurveyLink() {
+  return (
+      <StLink href={surveyLink} target="_blank" rel="noreferrer">
+        &gt; 피드백을 남겨주세요
+      </StLink>
+  );
 }
 
 const Wrap = styled.div`
   position: fixed;
   bottom: 0;
   padding: 1em;
+
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StLink = styled.a`
+  font-size: 8px;
+  font-weight: normal;
+  &hover {
+    font-weight: bold;
+  }
 `;
