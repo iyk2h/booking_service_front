4bb0185bf6726e42440e057480da6dd30141058e
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-05-07 17:19:06 +0900

    edit util function path

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,9 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
+import { fullDateFormatter } from "../utils/format";
 
 export function getReservedTime(fno, data) {
   return axios.post(`/booking/${fno}/date`, data);
 }
 
-export function postReserve(fno, data) {
-  return axios.post(`/booking/${fno}`, data)
+export function postReserve(fno, dateState, userPick) {
+  const data = {
+    date: fullDateFormatter(dateState),
+    maxHour: userPick.length,
+    selectedTime: userPick[0],
+  };
+  return axios.post(`/booking/${fno}`, data);
 }
